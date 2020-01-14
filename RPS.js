c57158c10c992a88c7aaa9504db9ef1c40ff8a86
KieranTheMan <kieran9fowler@gmail.com>
2020-01-14 16:38:34 +0000

    create eventListener for rock, paper, scissors and game function

--- RPS.js
+++ RPS.js
@@ -1,9 +1,26 @@
 let userScore = 0;
 let compScore = 0;
-let userScore = document.getElementById('user-score');
-let compScore = document.getElementById('comp-score');
+let userScore_span = document.getElementById('user-score');
+let compScore_span = document.getElementById('comp-score');
 let scoreBoard = document.querySelector('.score-board');
 let result = document.querySelector('.result');
 let rock = document.getElementById('r');
 let paper = document.getElementById('p');
 let scissors = document.getElementById('s');
+
+
+function game(userChoice) {
+    
+}
+
+rock.addEventListener('click', function() {
+game('r');
+});
+
+paper.addEventListener('click', function() {
+game('p');
+});
+
+scissors.addEventListener('click', function() {
+game('s');
+});
